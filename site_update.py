5d78b5f593622a1620d45a9fb1ad83b399126954
Dilum Ranatunga <dilum.ranatunga@wpengine.com>
2019-02-19 12:49:14 -0600

    Adds site updater. It should be run using the shell script.

---
+++ site_update.py
@@ -0,0 +1,55 @@
+from flask import Flask, request
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return 'Use POST /changed with colors body in JSON'
+
+@app.route('/changed', methods=['POST'])
+def changed():
+    body = request.data
+    print(body)
+    success = handle_color_spec(json.loads(body))
+    return json.dumps({'success':success}), (200 if success else 400), {'ContentType':'application/json'}
+
+
+block_content_cache = {}
+
+def get_block_content(name):
+    global block_content_cache
+    if name not in block_content_cache:
+        try:
+            with open(f'blocks/{name}.txt', 'r') as fh:
+                content = fh.read()
+                content = content.replace('\r', '')
+                content = content.replace('\n', '')
+        except IOError as x:
+            print(f'Could not open {name} -- {x}')
+            content = None
+        block_content_cache[name] = content
+        return content
+    return block_content_cache[name]
+
+
+def handle_color_spec(spec_colors):
+    cleaned = [(str(name).lower(), x, y) for name, x, y in spec_colors]
+    cleaned.sort(key=lambda t: t[2])
+    combined = ''
+    for name, x, y in cleaned:
+        content = get_block_content(name)
+        if not content:
+            return False
+        combined += content
+        combined += '  '  # the wp cli command likes having at least two spaces between blocks
+    enqueue_content_update(combined)
+    return True
+
+
+def enqueue_content_update(content):
+    post_number = 4
+    cmd = f"wp post update {post_number} --post_content='{content}'"
+    print(content)
+    os.system(cmd)
