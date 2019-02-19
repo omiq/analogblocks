4459fd6af8dbdaf6f538ab561027a53d0ef9e617
Dilum Ranatunga <dilum.ranatunga@wpengine.com>
2019-02-19 11:01:44 -0600

    Adds simple centroid formula. Now text output shows color ordered top to bottom.

--- color.py
+++ color.py
@@ -3,6 +3,34 @@ import numpy as np
 import cv2
 
 
+def unpack_triple_nested(tn):
+    '''
+    [[[x1, y1]], [x2, y2]], ...] --> [(x1, y1), (x2, y2), ...]
+    '''
+    tuples = []
+    for dn in tn:
+        for sn in dn:
+            tuples.append((sn[0], sn[1]))
+    return tuples
+
+
+def simple_centroid(points):
+    '''
+    Simple centroid without any outlier compensation
+    '''
+    count = 0
+    xsum = 0
+    ysum = 0
+    for (x, y) in points:
+        count += 1
+        xsum += x
+        ysum += y
+    if count:
+        return (xsum/count, ysum/count)
+    else:
+        return None
+
+
 # load the image
 image = cv2.imread("blocks.jpg")
 
@@ -16,6 +44,9 @@ boundaries = [
 ]
 
 # loop over the boundaries
+
+color_tuples = []
+
 for (name, lower, upper) in boundaries:
     # create NumPy arrays from the boundaries
     lower = np.array(lower, dtype = "uint8")
@@ -24,13 +55,16 @@ for (name, lower, upper) in boundaries:
     # find the colors within the specified boundaries and apply
     # the mask
     mask = cv2.inRange(image, lower, upper)
-    points = cv2.findNonZero(mask)
-    print(name)
-    print(points)
-    output = cv2.bitwise_and(image, image, mask=mask)
+    points = unpack_triple_nested(cv2.findNonZero(mask))
+    cen_x, cen_y = simple_centroid(points)
+    color_tuples.append((name, cen_x, cen_y))
 
     # show the images
+    # output = cv2.bitwise_and(image, image, mask=mask)
     #cv2.imshow("images", np.hstack([image, output]))
     #cv2.waitKey(0)
 
 cv2.destroyAllWindows()
+
+color_tuples.sort(key=lambda t: t[2])
+print(color_tuples)
